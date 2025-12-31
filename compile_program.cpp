1646f517285ce8e17804a9d93ed528eb21c21aa0
kakolla <abhishekkakolla1@gmail.com>
2025-12-31 00:40:07 -0500

    changed char code gen and removed empty arr

--- compile_program.cpp
+++ compile_program.cpp
@@ -11,14 +11,8 @@ std::string compile_program(int x) {
 }
 
 std::string compile_program(char x) {
-    std::string res = "mov w0, #";
-    res.push_back('\'');
-    res.push_back(x);
-    res.push_back('\'');
-    res += "\nret\n";
-    std::cout << res << std::endl;
-    return res;
-}
+    return "mov w0, #" + std::to_string((int)x) + "\nret\n";
+} 
 
 std::string compile_program(bool x) {
     std::string res = "mov w0, #" + std::to_string(static_cast<int>(x)) + "\nret\n";
@@ -26,17 +20,12 @@ std::string compile_program(bool x) {
     return res;
 }
 
-std::string compile_program(const int* arr, size_t size) {
-    if (size == 0) return "/* empty array — no code generated */\n";
-
-    return "";
-}
 
 
 
 int main() {
     // std::string asm_code = compile_program(31415); // generate assembly code in a file
-    std::string asm_code = compile_program(99);
+    std::string asm_code = compile_program('K');
     std::ofstream output_stream("c_entry.s");
     output_stream << ".globl _c_entry\n"
         << "_c_entry:\n"
